0fa1ff2f9ab8699e9b71601642871019978fe6d7
Zzarty <zevsling@gmail.com>
2023-04-03 20:02:31 +0300

    first

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
 import { DirectionalLight, Scene } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
-
 import './style.css'
 
 let header_text = document.querySelector(".header-text")
 let header_img = document.querySelector(".header-img")
 
-const envUrl = new URL('./img/abandoned_slipway_4k.hdr', import.meta.url).href
-const crossbowUrl = new URL('./models/crossbow_scene.glb', import.meta.url).href
+const envUrl = './project-web/img/abandoned_slipway_4k.hdr'
+const crossbowUrl = './project-web/models/crossbow_scene.glb'
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
